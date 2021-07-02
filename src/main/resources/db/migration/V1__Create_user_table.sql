57042d989026d3b6c72e3afd57079402f2a70a85
auctance <liuanqi0424@126.com>
2021-07-02 10:49:04 +0800

    add flyway commition support

---
+++ src/main/resources/db/migration/V1__Create_user_table.sql
@@ -0,0 +1,11 @@
+-- auto-generated definition
+create table USER
+(
+    ID           int auto_increment
+        primary key,
+    ACCOUNT_ID   varchar(100) null,
+    NAME         varchar(50)  null,
+    TOKEN        char(36)     null,
+    GMT_CREATE   bigint       null,
+    GMT_MODIFIED bigint       null
+);
