57fa245bbe4ea8e9ec69ddfa0242b71b0a24ca6e
auctance <liuanqi0424@126.com>
2021-09-01 21:47:07 +0800

    fix loginMustHaveCookie

--- src/main/java/life/majiang/community2/controller/IndexController.java
+++ src/main/java/life/majiang/community2/controller/IndexController.java
@@ -18,17 +18,22 @@ public class IndexController {
     @GetMapping("/") //the root fold, default represent
     public String index(HttpServletRequest request){
         Cookie[] cookies = request.getCookies();
-        for (Cookie cookie:cookies){
-            if (cookie.getName().equals("token")){
-                String token = cookie.getValue();
-                User user  =  userMapper.findByToken(token);
-                if (user!=null){
-                    request.getSession().setAttribute("user", user);
+        try {
+            for (Cookie cookie:cookies){
+                if (cookie.getName().equals("token")){
+                    String token = cookie.getValue();
+                    User user  =  userMapper.findByToken(token);
+                    if (user!=null){
+                        request.getSession().setAttribute("user", user);
 
+                    }
+                    break;
                 }
-                break;
             }
+        }catch (java.lang.NullPointerException e){
+            // do nothing
+        }finally {
+            return "index";
         }
-        return "index";
     }
 }
